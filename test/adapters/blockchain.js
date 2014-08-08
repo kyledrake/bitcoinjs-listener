51457d62bc69000434b452c96a9a82169172507d
Kyle Drake <kyle@kyledrake.net>
2014-08-07 17:08:52 -0700

    space indentation

--- test/adapters/blockchain.js
+++ test/adapters/blockchain.js
@@ -2,13 +2,13 @@ var assert = require('assert')
 var Blockchain = require('../../lib/adapters/blockchain')
 
 describe('Blockchain', function() {
-	describe('getBlockHeight', function() {
-		it('returns latest', function(done) {
-			var blockchain = new Blockchain()
-			blockchain.getBlockHeight(function(height) {
-				assert(height > 454740)
-				done()
-			})
-		})
-	})
+  describe('getBlockHeight', function() {
+    it('returns latest', function(done) {
+      var blockchain = new Blockchain()
+      blockchain.getBlockHeight(function(height) {
+        assert(height > 454740)
+        done()
+      })
+    })
+  })
 })
--- test/listener.js
+++ test/listener.js
@@ -2,38 +2,38 @@ var assert = require('assert')
 var Listener = require('../lib/listener')
 
 describe('Listener', function() {
-	describe('add', function() {
-		it('adds address', function() {
-			var listener = new Listener()
-			var addr = '1HRiAtD4zFv3rMuqNBLjCaeB5sv2G49qCB'
-			listener.add(addr)
-			assert.deepEqual(listener.addresses, [addr])
-		})
+  describe('add', function() {
+    it('adds address', function() {
+      var listener = new Listener()
+      var addr = '1HRiAtD4zFv3rMuqNBLjCaeB5sv2G49qCB'
+      listener.add(addr)
+      assert.deepEqual(listener.addresses, [addr])
+    })
 
-		it('fails for bad address', function() {
-			var listener = new Listener()
-			assert.throws(function() { listener.add('derp') }, /Invalid address/)
-		})
+    it('fails for bad address', function() {
+      var listener = new Listener()
+      assert.throws(function() { listener.add('derp') }, /Invalid address/)
+    })
 
-		it('adds for array', function() {
-			var addrs = [
-				'1Kfim7Jc4cE2YZoshAHzT5MyaCHDc84Tu1', 
-				'1NeSda9xQxEfdL4iYq1nUySgbURTcsKQfF'
-			]
+    it('adds for array', function() {
+      var addrs = [
+        '1Kfim7Jc4cE2YZoshAHzT5MyaCHDc84Tu1', 
+        '1NeSda9xQxEfdL4iYq1nUySgbURTcsKQfF'
+      ]
 
-			var listener = new Listener()
-			listener.add(addrs)
-			assert.deepEqual(listener.addresses, addrs)
-		})
+      var listener = new Listener()
+      listener.add(addrs)
+      assert.deepEqual(listener.addresses, addrs)
+    })
 
-		it('fails for bad address in array', function() {
-			var addrs = [
-				'1Kfim7Jc4cE2YZoshAHzT5MyaCHDc84Tu1', 
-				'derp'
-			]
+    it('fails for bad address in array', function() {
+      var addrs = [
+        '1Kfim7Jc4cE2YZoshAHzT5MyaCHDc84Tu1', 
+        'derp'
+      ]
 
-			var listener = new Listener()
-			assert.throws(function() { listener.add('derp') }, /Invalid address/)
-		})
-	})
+      var listener = new Listener()
+      assert.throws(function() { listener.add('derp') }, /Invalid address/)
+    })
+  })
 })
